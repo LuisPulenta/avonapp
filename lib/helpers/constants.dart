b5ce4e25f744209a83a35fd20b0dcdaa8fcfde97
LuisPulenta <luisalbertonu@gmail.com>
2022-03-02 15:57:34 -0300

    Version 5

--- lib/helpers/constants.dart
+++ lib/helpers/constants.dart
@@ -1,7 +1,5 @@
 class Constants {
-  // static String get apiUrl => 'http://keypress.serveftp.net:609';
-  static String get apiUrl => 'http://keypress.serveftp.net:91';
-
+  static String get apiUrl => 'http://keypress.serveftp.net:609';
   static String get Modulo => 'Avon';
 
   static String get version => '1.0.0';
